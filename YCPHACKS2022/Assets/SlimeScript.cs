ab49306b7dba3024e8b041ebfbc4860c32d6146f
DakotaM1194 <dmessner@ycp.edu>
2022-11-05 00:13:23 -0400

    Start of slime stuffs

---
+++ YCPHACKS2022/Assets/SlimeScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeScript : MonoBehaviour
+{
+    public float health = 1;
+
+
+    public float Health{
+        set{
+            health = value;
+
+            if(health<= 0){
+                Defeated();
+            }
+        }
+        get{
+            return health;
+        }
+    }
+    public void Defeated(){
+        Destroy(gameObject);
+    }
+}
